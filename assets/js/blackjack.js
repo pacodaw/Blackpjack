7d5966759ae9e483dd280d5275c6a252f37c19d0
Paco Grande <pacseg13@gmail.com>
2023-08-09 21:18:22 +0200

    3nd, pedirCarta() return string & valorCarta() convert to number value

--- assets/js/blackjack.js
+++ assets/js/blackjack.js
@@ -15,7 +15,7 @@ const notNumCarts = ['J','Q','K','A'];
 // Create a new deck
 const createDeck = () => {
 
-    for (let i = 2; i <= 10; i++) {
+    for ( let i = 2; i <= 10; i++ ) {
         
         for ( let palo of palosBaraja ) {
            
@@ -32,7 +32,7 @@ const createDeck = () => {
     }
     //Returns a shuffled copy of the deck
     deck = _.shuffle( deck );
-    console.log(deck);
+    // console.log( deck );
     return deck;
 } 
 
@@ -40,7 +40,56 @@ createDeck();
 
 
 const pedirCarta = () => {  
-    console.log (deck);
 
-   
+    if (deck.length === 0) {
+        throw 'No quedan cartas en el deck paayo ==, game over';
+    }
+
+    const carta = deck.pop();
+    // console.log( carta );
+    // console.log( deck );
+    return carta;
 }
+
+let carta = pedirCarta();
+console.log( carta );
+
+const valorCarta = ( carta ) => {
+
+    // string carts exception of 7 caract length '10D.png'
+    // carta.charAt(0) === carta[0]
+    let valor = (carta.length === 6 ) ? carta.charAt(0) : carta.substring(0,2);
+    // === '10' 'J' || 'Q' || 'K' || 'A'
+
+        switch ( valor ) {
+
+            case '10':
+                valor = 10;
+                break;
+
+            case 'J':
+                valor = 10;
+                break;
+
+            case 'Q':
+                valor = 11;
+                break;
+
+            case 'K':
+                valor = 12;
+                break;
+
+            case 'A':
+                valor = 1;
+                break;
+        }
+
+    valor *= 1;     // Convert character (values 2 to 9) to number type
+    return valor;
+}
+
+carta = valorCarta ( carta );
+console.log( carta );
+
+
+/* const turnoJugador = () => {} */
